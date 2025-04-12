5f0098ca7ebcad1fd8bbd08ed77c664aba77546e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-12 12:11:27 -0400

    Update README.md

---
+++ be-flashy.js
@@ -0,0 +1,8 @@
+// @ts-check
+import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-flashy/types' */;
+
+class BeFlashy extends BE {
+}

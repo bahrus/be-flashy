554c036cee8892ebb8cd21da905f33fb022e3a75
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-12 12:32:01 -0400

    Update README.md

--- be-flashy.js
+++ be-flashy.js
@@ -4,5 +4,17 @@ import { BE } from 'be-enhanced/BE.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
 /** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-flashy/types' */;
 
+/**
+ * @implements {Actions}
+ * 
+ */
 class BeFlashy extends BE {
+    /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
+     */
+    static config = {
+    };
 }
+
+await BeFlashy.bootUp();
+export { BeFlashy as default};

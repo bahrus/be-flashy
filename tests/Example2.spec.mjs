16e172157dd65b9b3009e2db7fbb4b2c03e2e746
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-12 14:56:13 -0400

    Update README.md

---
+++ tests/Example2.spec.mjs
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+test('Example2', async ({ page }) => {
+    await page.goto('./tests/Example2.html');
+    // wait for 1 second
+    await page.waitForTimeout(2000);
+    const editor = page.locator('#target');
+    await expect(editor).toHaveAttribute('mark', 'good');
+});
